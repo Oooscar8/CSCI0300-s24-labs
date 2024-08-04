825e3ecb40393e6495f28ad7e22a134ddaba4ddf
Alex Sun <oscar8@sjtu.edu.cn>
2024-08-04 09:50:02 +0000

    almost done with lab1  assignment2

--- lab1/test_reverse.c
+++ lab1/test_reverse.c
@@ -14,67 +14,75 @@ static int num_incorrect = 0;
  * checks if the elements are properly reversed and returns an
  * error message if not
  */
-void assert_equal(char** expected, char** actual, const char* message,
-                  int len) {
+void assert_equal(char **expected, char **actual, const char *message,
+                  int len)
+{
   total_tests++;
   int element_correct = 0;
   int element_incorrect = 0;
 
-  for (int i = 0; i < len; i++) {
-    if (!strcmp(expected[i], actual[i])) {
+  for (int i = 0; i < len; i++)
+  {
+    if (!strcmp(expected[i], actual[i]))
+    {
       element_correct++;
-      if (element_correct == len) num_correct++;
-    } else {
+      if (element_correct == len)
+        num_correct++;
+    }
+    else
+    {
       element_incorrect++;
-      if (element_correct == len) num_incorrect++;
-      printf("\033[0;31m"); //set color to red
+      if (element_correct == len)
+        num_incorrect++;
+      printf("\033[0;31m"); // set color to red
       printf(
           "[FAILED Test %d: %s]: At index: %d, expected string: %s, actual "
           "string: %s\n",
           total_tests, message, i, expected[i], actual[i]);
-      printf("\033[0m"); //reset text to default color
+      printf("\033[0m"); // reset text to default color
     }
   }
 
-  //If no elements are wrong, the test passed! Print a message that says this :)
-  if (element_incorrect == 0){
-    printf("\033[0;32m"); //set color to green
+  // If no elements are wrong, the test passed! Print a message that says this :)
+  if (element_incorrect == 0)
+  {
+    printf("\033[0;32m"); // set color to green
     printf("[PASSED Test %d: %s]\n", total_tests, message);
-    printf("\033[0m"); //reset text to default color
+    printf("\033[0m"); // reset text to default color
   }
-
 }
 
 /**
  * Runs tests for student implementation of reverse
  */
-void test_reverse() {
+void test_reverse()
+{
   // test 1: test single element
-  char* str[] = {"hello"};
+  char *str[] = {"hello"};
   reverse_arr(str, 1);
   assert_equal(str, str, "one element test", 1);
 
   // test 2: test three elements (odd number of elements)
-  char* str2[] = {"hello", "my", "name"};
-  char* reverse2[] = {"name", "my", "hello"};
+  char *str2[] = {"hello", "my", "name"};
+  char *reverse2[] = {"name", "my", "hello"};
   reverse_arr(str2, 3);
   assert_equal(str2, reverse2, "three element test", 3);
 
   // test 3: test four elements (even number of elements)
-  char* str3[] = {"hello", "my", "name", "is"};
-  char* reverse3[] = {"is", "name", "my", "hello"};
+  char *str3[] = {"hello", "my", "name", "is"};
+  char *reverse3[] = {"is", "name", "my", "hello"};
   reverse_arr(str3, 4);
   assert_equal(str3, reverse3, "four element test", 4);
 
   // test 4: test five elements
-  char* str4[] = {"i", "love", "systems", "csci", "300"};
-  char* reverse4[] = {"300", "csci", "systems", "love", "i"};
+  char *str4[] = {"i", "love", "systems", "csci", "300"};
+  char *reverse4[] = {"300", "csci", "systems", "love", "i"};
   reverse_arr(str4, 5);
   assert_equal(str4, reverse4, "five element test", 5);
 
   // test 5: test 2 elements
-  char* str5[] = {"csci", "300"};
-  char* reverse5[] = {"300", "csci"};
+  char *str5[] = {"csci", "300"};
+  char *reverse5[] = {"300", "csci"};
   reverse_arr(str5, 2);
   assert_equal(str5, reverse5, "two element test", 2);
 
@@ -91,7 +99,8 @@ void test_reverse() {
 /**
  * prints a summary based on the number of tests passed and failed
  */
-void print_test_summary() {
+void print_test_summary()
+{
   printf("Passed %d tests, Failed %d tests | %f%%\n", num_correct,
          num_incorrect,
          100 * ceil((float)num_correct) / (total_tests == 0 ? 1 : total_tests));
@@ -103,7 +112,8 @@ void print_test_summary() {
 /**
  * runs the function passed in and prints a test summary
  */
-void run_test(void (*func)(), const char* message) {
+void run_test(void (*func)(), const char *message)
+{
   printf("=================== Running %s Tests. ===================\n",
          message);
   func();
@@ -127,20 +137,31 @@ void run_test(void (*func)(), const char* message) {
  *
  * i.e. argv[1] --> number of elements, thus argc - 2 == atoi(argv[1])
  */
-int main(int argc, char** argv) {
-  int num_elements = atoi(argv[1]);
-
-  char* arr[num_elements];
-  for (int i = 0; i < num_elements; i++) {
-    arr[i] = argv[i + 2];
+int main(int argc, char **argv)
+{
+  if (atoi(argv[1]) == argc - 1)
+  {
+    // 触发一个错误，退出状态码为 1
+    fprintf(stderr, "Error: The number of elements inputted should correspond to the number of elements to be reversed.\n");
+    return 1;
   }
 
-  reverse_arr(arr, num_elements);
+  if (argc > 1)
+  {
+    char *arr[argc];
+    for (int i = 0; i < argc; i++)
+    {
+      arr[i] = argv[i + 2];
+    }
 
-  for (int i = 0; i < num_elements; i++) {
-    printf("%s ", arr[i]);
+    reverse_arr(arr, argc);
+
+    for (int i = 0; i < argc; i++)
+    {
+      printf("%s ", arr[i]);
+    }
+    printf("\n");
   }
-  printf("\n");
 
   printf("Testing for correctness...\n");
   // passes the function test_reverse to run_test
